d3dad1e043be7247ef79a7ecdee5682f170a5e39
mtegarsp <tganteng77@gmail.com>
2019-05-06 00:49:24 +0700

    s

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -7,6 +7,7 @@ const sourceMapEnabled = isProduction
   : config.dev.cssSourceMap
 
 module.exports = {
+  publicPath: '/'
   loaders: utils.cssLoaders({
     sourceMap: sourceMapEnabled,
     extract: isProduction
