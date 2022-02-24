babe901d47d2e499c4deb78892edafa77c027f34
Arsan <arsan.personal@gmail.com>
2022-02-24 12:42:27 +0800

    feat(projects): loading while requesting

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -2,8 +2,7 @@
   <div>
     <section class="project section" id="project">
       <h2 class="section-title">my project.</h2>
-
-      <div class="project__container bd-grid">
+      <div v-if="projects.length > 0" class="project__container bd-grid">
         <div v-for="(project, index) in projects" :key="index" class="project__img">
           <div class="project__img_bg" :style="`background: url('${$env.VUE_APP_PROJECT_URL}/${project.thumbnail}'); width: 100%; height:200px; background-size: cover; background-position: center;`">
             <router-link tag="a" :to="{name: 'projectdetail', params: {dir: project.folderName.replaceAll(' ', '-')}}" class="project__link">
@@ -12,21 +11,30 @@
           </div>
         </div>
       </div>
+      <div v-else class="bd-grid">
+        <lottie :options="lottieLoadingOptions"/>
+      </div>
     </section>
   </div>
 </template>
 <script>
+import lottie from 'vue-lottie'
+import lottieLoading from '@/assets/lottie/loading.json'
+
 export default {
+  components: {
+    lottie,
+  },
   data() {
     return {
-      projects: []
+      projects: [],
+      lottieLoadingOptions: {animationData: lottieLoading}
     }
   },
   methods: {
     async getProjects() {
       let projects = await this.$axios.get(`${this.$env.VUE_APP_PROJECT_URL}/getFolders`)
       this.projects = projects.data
-      console.log(this.projects)
     }
   },
   mounted() {
